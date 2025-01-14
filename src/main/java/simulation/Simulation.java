84a0957dafce4c0782707ef538af0869f19488bd
nikita-webdev <nikita1722@bk.ru>
2025-01-15 00:23:26 +0700

    first commit

---
+++ src/main/java/simulation/Simulation.java
@@ -0,0 +1,19 @@
+package simulation;
+
+public class Simulation {
+    public static void main(String[] args) {
+
+    }
+
+    void nextTurn() {
+
+    }
+
+    void startSimulation() {
+
+    }
+
+    void pauseSimulation() {
+
+    }
+}
